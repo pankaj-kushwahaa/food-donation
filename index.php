dc6b2dbf0201d99df09c331dd3fca9b5ce516e7a
Pankaj <pankajkushwaha251@gmail.com>
2022-11-26 21:57:34 +0530

    second commit

--- index.php
+++ index.php
@@ -8,103 +8,105 @@
         
     </head>
 <body>
-    
-    <header class="text-gray-600 body-font">
-        <header style="background-color:#5658df;"></body>
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-            </svg>
-            <span class="ml-3 text-xl text-white">Donation Management</span>
-          </a>
-          <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+  <header class="text-gray-600 body-font">
+      <header style="background-color:#5658df;">
+      <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span class="ml-3 text-xl text-white">Donation Management</span>
+        </a>
+        <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
 
-            <a href="index.php" class="mr-5 hover:text-white-900 text-white">Home</a>
-            <a href="Donation.php" class="mr-5 hover:text-white-900 text-white">Donate</a>
-            <a href="mission.php" class="mr-5 hover:text-white-900 text-white">Mission</a>
-            <a href="about.php" class="mr-5 hover:text-white-900 text-white">About</a>
-          </nav>
-                      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <a href="index.php" class="mr-5 hover:text-white-900 text-white">Home</a>
+          <a href="Donation.php" class="mr-5 hover:text-white-900 text-white">Donate</a>
+          <a href="mission.php" class="mr-5 hover:text-white-900 text-white">Mission</a>
+          <a href="about.php" class="mr-5 hover:text-white-900 text-white">About</a>
+        </nav>
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+            <path d="M5 12h14M12 5l7 7-7 7"></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  </header>
+
+  <section class="text-gray-600 body-font">
+    <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+        <img class="object-cover object-center rounded" alt="hero" src="https://www.vmcdn.ca/f/files/shared/miscellaneous-stock-images/food-donation.jpeg;w=960">
+      </div>
+      <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+        <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          <br class="hidden lg:inline-block">Alone We Can Do Little Together We Can Do So Much
+        </h1>
+        <p class="mb-8 leading-relaxed">Giving Is Not Only About Donation It Also About Making Change</p>
+        <div class="flex justify-center">
+          <a href="Donation.php" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Now</a>
         </div>
-        <body>
-      </header>
-      <section class="text-gray-600 body-font">
-        <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-            <img class="object-cover object-center rounded" alt="hero" src="https://www.vmcdn.ca/f/files/shared/miscellaneous-stock-images/food-donation.jpeg;w=960">
-          </div>
-          <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-              <br class="hidden lg:inline-block">Alone We Can Do Little Together We Can Do So Much
-            </h1>
-            <p class="mb-8 leading-relaxed">Giving Is Not Only About Donation It Also About Making Change</p>
-            <div class="flex justify-center">
-              <a href="Donation.php
-" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Now</a>
-            
-</div>
-          </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="text-gray-600 body-font">
+    <section style="background-color:#8db2ea;">
+
+      <div class="container px-5 py-24 mx-auto">
+        <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
+          <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">We Manage Donation Like</h1>
+          <p class="lg:w-1/2 w-full leading-relaxed text-white-700">Food, Cloth and Money</p>
         </div>
-        </section>
-        <section class="text-gray-600 body-font">
-          <section style="background-color:#8db2ea;"></body>
-            <div class="container px-5 py-24 mx-auto">
-                <div class="flex flex-wrap w-full mb-20 flex-col items-center text-center">
-                <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">We Manage Donation Like</h1>
-                <p class="lg:w-1/2 w-full leading-relaxed text-gray-500"></p>
+        
+        <div class="flex flex-wrap -m-4">
+          
+          <div class="xl:w-1/3 md:w-1/2 p-4">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                </svg>
               </div>
-              <div class="flex flex-wrap -m-4">
-                <div class="xl:w-1/3 md:w-1/2 p-4">
-                  <div class="border border-gray-200 p-6 rounded-lg">
-                    <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-                        <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-                      </svg>
-               </div>
-                    <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Food</h2>
-                    <p class="leading-relaxed text-base">Feed The Needy</p>
-                    <a href="Donation.php
-      " class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Now</a>
-
-                  </div>
-                </div>
-                <div class="xl:w-1/3 md:w-1/2 p-4">
-                  <div class="border border-gray-200 p-6 rounded-lg">
-                    <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-                      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-                        <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                        <circle cx="12" cy="7" r="4"></circle>
-                      </svg>
-                    </div>
-                    <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Money</h2>
-                    <p class="leading-relaxed text-base">Help The Poor</p>
-                    <a href="money.php
-      " class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Now</a>
-                  </div>
-</div>
-      <div class="xl:w-1/3 md:w-1/2 p-4">
-        <div class="border border-gray-200 p-6 rounded-lg">
-          <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
-              <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-              <circle cx="12" cy="7" r="4"></circle>
-            </svg>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Food</h2>
+              <p class="leading-relaxed text-base">Feed The Needy</p>
+              <a href="Donation.php" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Now</a>
+            </div>
           </div>
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Clothes</h2>
-          <p class="leading-relaxed text-base">Someone Needs You</p>
-<a href="clothes.php" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Now</a>
 
-<br><br>
-<input type="datetime" name="" id=""> <br>
-<input type="date" name="" id="">
- 
+          <div class="xl:w-1/3 md:w-1/2 p-4">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+              </div>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Money</h2>
+              <p class="leading-relaxed text-base">Help The Poor</p>
+              <a href="money.php" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Now</a>
+            </div>
+          </div>
+
+          <div class="xl:w-1/3 md:w-1/2 p-4">
+            <div class="border border-gray-200 p-6 rounded-lg">
+              <div class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-6 h-6" viewBox="0 0 24 24">
+                  <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                  <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+              </div>
+              <h2 class="text-lg text-gray-900 font-medium title-font mb-2">Clothes</h2>
+              <p class="leading-relaxed text-base">Someone Needs You</p>
+              <a href="clothes.php" class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Donate Now</a>
+            </div>
+          </div>
+
         </div>
-                      
-              
-     </body>
+
+    </section>
+  </section>
+                        
+</body>
+</html>
 
             
